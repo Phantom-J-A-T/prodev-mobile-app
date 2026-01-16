b5b8ecd9c1a7b18345b570ab06da6be9f0a3e3d2
Phantom-J-A-T <ramonadedotun550@gmail.com>
2026-01-16 15:55:16 +0100

    Updating the Property cards

---
+++ prodev-mobile-app-0x05/interfaces.ts
@@ -0,0 +1,21 @@
+// Define your interfaces here
+
+
+export interface PropertyListingProps {
+    id: string;
+    title: string;
+    description: string;
+    price: number;
+    imageUrl: string;
+    Property: string;
+    propertyName: string;
+    rate: number;
+    currency: number;
+    amount: number;
+    location: string;
+    favorite: string;
+    street: string;
+    
+
+    // Add other properties as needed
+}
